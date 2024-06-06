7d329de90a3d59b904846859b27d9bb22505f8ff
Elie MEGNANE NGUEMA <92931214+Elijah30@users.noreply.github.com>
2024-06-06 14:51:05 +0800

    Update errors4.rs
    La méthode PositiveNonzeroInteger::new actuelle retourne toujours Ok, même lorsqu'elle reçoit un entier négatif ou zéro en argument. Cela est dû à l'utilisation de la conversion de type value as u64, qui ne vérifie pas si value est négatif ou nul.

--- exercises/13_error_handling/errors4.rs
+++ exercises/13_error_handling/errors4.rs
@@ -9,24 +9,17 @@
 struct PositiveNonzeroInteger(u64);
 
 #[derive(PartialEq, Debug)]
-enum CreationError {
-    Negative,
-    Zero,
-}
-
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
-        // Hmm... Why is this always returning an Ok value?
-        Ok(PositiveNonzeroInteger(value as u64))
+        if value > 0 {
+            Ok(PositiveNonzeroInteger(value as u64))
+        } else if value == 0 {
+            Err(CreationError::Zero)
+        } else {
+            Err(CreationError::Negative)
+        }
     }
 }
 
-#[test]
-fn test_creation() {
-    assert!(PositiveNonzeroInteger::new(10).is_ok());
-    assert_eq!(
-        Err(CreationError::Negative),
-        PositiveNonzeroInteger::new(-10)
-    );
     assert_eq!(Err(CreationError::Zero), PositiveNonzeroInteger::new(0));
 }
