9366fb80229306df7a6fad54250560d16b1c2db2
Elie MEGNANE NGUEMA <92931214+Elijah30@users.noreply.github.com>
2024-06-06 13:57:02 +0800

    Update move_semantics4.rs
    pour remplir un vecteur à l'intérieur de la fonction fill_vec sans passer de vecteur en argument, vous devez d'abord créer un nouveau vecteur et le remplir avec les valeurs nécessaires. Vous pouvez utiliser la méthode vec![] pour créer un nouveau vecteur et spécifier les valeurs initiales.

--- exercises/06_move_semantics/move_semantics4.rs
+++ exercises/06_move_semantics/move_semantics4.rs
@@ -13,17 +13,16 @@
 fn main() {
     let vec0 = vec![22, 44, 66];
 
-    let vec1 = fill_vec(vec0);
+    let vec1 = fill_vec();
 
     assert_eq!(vec1, vec![22, 44, 66, 88]);
 }
 
-// `fill_vec()` no longer takes `vec: Vec<i32>` as argument - don't change this!
 fn fill_vec() -> Vec<i32> {
-    // Instead, let's create and fill the Vec in here - how do you do that?
-    let mut vec = vec;
-
-    vec.push(88);
+    let mut vec = vec![22, 44, 66]; // Créez un nouveau vecteur avec les valeurs initiales
+    vec.push(88); // Ajoutez la nouvelle valeur à la fin du vecteur
+    vec // Renvoyez le vecteur rempli
+}
 
     vec
 }
