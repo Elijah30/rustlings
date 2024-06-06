a04b237bab263eed75d770e90c7168dc0fd64f49
Elie MEGNANE NGUEMA <92931214+Elijah30@users.noreply.github.com>
2024-06-06 14:12:20 +0800

    Update enums2.rs
    Pour définir l'énumération Message avec les différentes variantes utilisées dans la fonction main, vous pouvez utiliser les variantes suivantes :
    
        Move { x: i32, y: i32 } : Une variante qui contient une struct avec deux champs x et y.
        Echo(String) : Une variante qui contient une String.
        ChangeColor(u8, u8, u8) : Une variante qui contient trois valeurs u8 représentant les composantes de couleur (rouge, vert, bleu).
        Quit : Une variante qui ne contient aucune donnée.

--- exercises/08_enums/enums2.rs
+++ exercises/08_enums/enums2.rs
@@ -5,26 +5,12 @@
 
 // I AM NOT DONE
 
-#[derive(Debug)]
-enum Message {
-    // TODO: define the different variants used below
-}
+Pour définir l'énumération Message avec les différentes variantes utilisées dans la fonction main, vous pouvez utiliser les variantes suivantes :
 
-impl Message {
-    fn call(&self) {
-        println!("{:?}", self);
-    }
-}
-
-fn main() {
-    let messages = [
-        Message::Move { x: 10, y: 30 },
-        Message::Echo(String::from("hello world")),
-        Message::ChangeColor(200, 255, 255),
-        Message::Quit,
-    ];
-
-    for message in &messages {
+    Move { x: i32, y: i32 } : Une variante qui contient une struct avec deux champs x et y.
+    Echo(String) : Une variante qui contient une String.
+    ChangeColor(u8, u8, u8) : Une variante qui contient trois valeurs u8 représentant les composantes de couleur (rouge, vert, bleu).
+    Quit : Une variante qui ne contient aucune donnée. in &messages {
         message.call();
     }
 }
