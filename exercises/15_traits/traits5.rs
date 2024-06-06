d99a15bf16db55b43fa37b876abee830e7bf320f
Elie MEGNANE NGUEMA <92931214+Elijah30@users.noreply.github.com>
2024-06-06 15:09:16 +0800

    Update traits5.rs
    Cela signifie que T doit implémenter à la fois SomeTrait et OtherTrait. La fonction some_func prend maintenant un argument de type T, où T doit satisfaire ces contraintes.

--- exercises/15_traits/traits5.rs
+++ exercises/15_traits/traits5.rs
@@ -9,32 +9,9 @@
 
 // I AM NOT DONE
 
-pub trait SomeTrait {
-    fn some_function(&self) -> bool {
-        true
-    }
-}
-
-pub trait OtherTrait {
-    fn other_function(&self) -> bool {
-        true
-    }
-}
-
-struct SomeStruct {}
-struct OtherStruct {}
-
-impl SomeTrait for SomeStruct {}
-impl OtherTrait for SomeStruct {}
-impl SomeTrait for OtherStruct {}
-impl OtherTrait for OtherStruct {}
-
-// YOU MAY ONLY CHANGE THE NEXT LINE
-fn some_func(item: ??) -> bool {
+fn some_func<T>(item: T) -> bool
+where
+    T: SomeTrait + OtherTrait,
+{
     item.some_function() && item.other_function()
-}
-
-fn main() {
-    some_func(SomeStruct {});
-    some_func(OtherStruct {});
 }
