d1e481f6fafd2b2a1a17342f1195c9637411aba3
Elie MEGNANE NGUEMA <92931214+Elijah30@users.noreply.github.com>
2024-06-06 16:23:02 +0800

    Update quiz1.rs
    Fonction calculate_price_of_apples : Calcule le prix en fonction du nombre de pommes achetées. Si le nombre est inférieur ou égal à 40, le prix est de 2 unités par pomme. Si le nombre est supérieur à 40, le prix est de 1 unité par pomme.
    Module de test : Contient un test verify_test qui vérifie que la fonction calcule correctement le prix des pommes pour différents nombres de pommes achetées.

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -20,14 +20,31 @@
 
 // Don't modify this function!
 #[test]
-fn verify_test() {
-    let price1 = calculate_price_of_apples(35);
-    let price2 = calculate_price_of_apples(40);
-    let price3 = calculate_price_of_apples(41);
-    let price4 = calculate_price_of_apples(65);
+fn calculate_price_of_apples(quantity: u32) -> u32 {
+    if quantity <= 40 {
+        quantity * 2
+    } else {
+        quantity
+    }
+}
 
-    assert_eq!(70, price1);
-    assert_eq!(80, price2);
-    assert_eq!(41, price3);
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn verify_test() {
+        let price1 = calculate_price_of_apples(35);
+        let price2 = calculate_price_of_apples(40);
+        let price3 = calculate_price_of_apples(41);
+        let price4 = calculate_price_of_apples(65);
+
+        assert_eq!(70, price1);
+        assert_eq!(80, price2);
+        assert_eq!(41, price3);
+        assert_eq!(65, price4);
+    }
+}
+(41, price3);
     assert_eq!(65, price4);
 }
