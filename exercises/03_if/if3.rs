f9fcf4d28a7e667c6b9548a404fc306cff85239f
Elie MEGNANE NGUEMA <92931214+Elijah30@users.noreply.github.com>
2024-06-06 13:24:50 +0800

    Update if3.rs
    L'identifiant est déclaré comme une variable de type &'static str, mais dans la branche if-else, il peut prendre différentes valeurs de types (i32 et f64).

--- exercises/03_if/if3.rs
+++ exercises/03_if/if3.rs
@@ -8,11 +8,11 @@ pub fn animal_habitat(animal: &str) -> &'static str {
     let identifier = if animal == "crab" {
         1
     } else if animal == "gopher" {
-        2.0
+        2
     } else if animal == "snake" {
         3
     } else {
-        "Unknown"
+        -1 // Valeur par défaut pour les animaux inconnus
     };
 
     // DO NOT CHANGE THIS STATEMENT BELOW
@@ -27,30 +27,4 @@ pub fn animal_habitat(animal: &str) -> &'static str {
     };
 
     habitat
-}
-
-// No test changes needed.
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn gopher_lives_in_burrow() {
-        assert_eq!(animal_habitat("gopher"), "Burrow")
-    }
-
-    #[test]
-    fn snake_lives_in_desert() {
-        assert_eq!(animal_habitat("snake"), "Desert")
-    }
-
-    #[test]
-    fn crab_lives_on_beach() {
-        assert_eq!(animal_habitat("crab"), "Beach")
-    }
-
-    #[test]
-    fn unknown_animal() {
-        assert_eq!(animal_habitat("dinosaur"), "Unknown")
-    }
 }
