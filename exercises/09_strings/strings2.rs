f4b08b43df1722b0de0c28b285978a30f734e767
Elie MEGNANE NGUEMA <92931214+Elijah30@users.noreply.github.com>
2024-06-06 14:22:47 +0800

    Update strings2.rs
    Dans votre code, la fonction is_a_color_word attend une référence de type &str, mais l'argument que vous passez est une instance de String. Pour corriger cela, vous devez passer une référence à la chaîne de caractères.

--- exercises/09_strings/strings2.rs
+++ exercises/09_strings/strings2.rs
@@ -9,7 +9,7 @@
 
 fn main() {
     let word = String::from("green"); // Try not changing this line :)
-    if is_a_color_word(word) {
+    if is_a_color_word(&word) { // Pass a reference to the string
         println!("That is a color word I know!");
     } else {
         println!("That is not a color word I know.");
@@ -18,4 +18,6 @@ fn main() {
 
 fn is_a_color_word(attempt: &str) -> bool {
     attempt == "green" || attempt == "blue" || attempt == "red"
+}
+ttempt == "green" || attempt == "blue" || attempt == "red"
 }
